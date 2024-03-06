694613ae7353f516470e97344f6ff87ce13cb79a
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-03-06 18:10:14 -0400

    spring boot cake-shop init

---
+++ src/main/java/br/com/mfsdevsystem/cakes/controller/AdminController.java
@@ -0,0 +1,5 @@
+package br.com.mfsdevsystem.cakes.controller;
+
+public class AdminController {
+
+}
---
+++ src/test/java/br/com/mfsdevsystem/cakes/CakesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mfsdevsystem.cakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
